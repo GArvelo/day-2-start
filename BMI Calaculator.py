e5e2d92446ee40b195582ddf1128b83457ca662d
Geraldo Arvelo <119348946+GArvelo@users.noreply.github.com>
2023-03-03 21:34:15 +0000

    Day 2

--- BMI Calaculator.py
+++ BMI Calaculator.py
@@ -5,14 +5,17 @@ weight = input("enter your weight in kg: ")
 h = float(height)
 w = int(weight)
 
-print(int(w /(h ** 2)))
+print(int(w / (h**2)))
 
 #great example with explanations
 
+weight_as_int = int(weight)
+height_as_float = float(height)
+
 #Using the exponent operator
-bmi = weight_as_int / height_as_float **2
-# or using multiplication and PEMDA
-bmi = weight_as_in / (height_as_float * height_as_float)
+bmi = weight_as_int / height_as_float**2
+#  or using multiplication and PEMDA
+bmi = weight_as_int / (height_as_float * height_as_float)
 
 bmi_as_int = int(bmi)
 
